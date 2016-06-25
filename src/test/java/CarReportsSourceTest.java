219b4334a2c620fd683cd371ee3fd92dfa5accf2
Ignacio N. Lucero Ascencio <igwisnirrol@gmail.com>
2016-06-25 22:11:07 +0200

    First stable working version.

--- src/test/java/CarReportsSourceTest.java
+++ src/test/java/CarReportsSourceTest.java
@@ -1,8 +1,15 @@
 import es.houcros.linearroad.datasource.CarReportsSource;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.AllWindowedStream;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.windowing.AllWindowFunction;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
 import org.junit.Test;
 
 /**
@@ -18,8 +25,13 @@ public class CarReportsSourceTest {
         env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
 
         DataStream<String> reports = env.addSource(new CarReportsSource<String>(inputFile)).returns(String.class);
-        reports.timeWindowAll(Time.seconds(2));
-        reports.print();
+        AllWindowedStream<String, TimeWindow> b = reports.windowAll(TumblingEventTimeWindows.of(Time.seconds(3)));
+        b.apply(new AllWindowFunction<String, Object, TimeWindow>() {
+            @Override
+            public void apply(TimeWindow timeWindow, Iterable<String> iterable, Collector<Object> collector) throws Exception {
+                collector.collect("Notif");
+            }
+        }).print();
 
         env.execute("CarReportsSourceTest");
     }
